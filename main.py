af3a8f11364f3e93cb1f0a72a3efe2ba5871a4d1
Dmitriy <skull18@ngs.ru>
2019-03-10 21:40:54 +0700

    updated idx reader as mnist dataset reader

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-import idxreader
+import mnist
 
-idxreader.read("train-images.idx3-ubyte")
+imgs, ans = mnist.open_dataset("train_imgs.idx", "train_labels.idx")
+
+with open("out.txt", "w") as f:
+	for img in enumerate(imgs[:50]):
+		for x in enumerate(img[1]):
+			if x[0] % 28 == 0:
+				f.write("\n\n")
+			f.write("%s" % ("#" if x[1] != 0 else "-"))
+		print(int.from_bytes(ans[img[0]], "big"), end = ", ")
